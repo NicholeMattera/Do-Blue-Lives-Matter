534b07e1e64cd4dfecb40fe0401a7afc567b039e
Nichole Mattera <me@nicholemattera.gay>
2020-07-21 19:32:49 -0400

    Redirect fix.

--- main.go
+++ main.go
@@ -5,17 +5,21 @@ import (
 	"net/http"
 )
 
-func index(w http.ResponseWriter, req * http.Request) {
-	if req.URL.Path != "/smatter" {
-		http.Redirect(w, req, "https://" + req.Host + "/smatter", http.StatusPermanentRedirect)
-		return
-	}
-
-	http.ServeFile(w, req, "./static/index.html")
-}
-
 func main() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/smatter" {
+			scheme := "http"
+			if req.TLS != nil {
+				scheme += "s"
+			}
+
+			http.Redirect(w, req, scheme + "://" + req.Host + "/smatter", http.StatusPermanentRedirect)
+			
+			return
+		}
+	
+		http.ServeFile(w, req, "./static/index.html")
+	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
