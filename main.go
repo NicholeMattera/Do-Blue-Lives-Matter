836c7de946d2e5f64c2dd8bbae26805ebe3ca609
Nichole Mattera <me@nicholemattera.gay>
2020-07-22 09:07:21 -0400

    Added Easter Egg.

--- main.go
+++ main.go
@@ -3,11 +3,39 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 )
 
+type TemplateData struct {
+	Title		string
+	Description	string
+	URL			string
+}
+
 func main() {
+	mainTemplate, err := template.ParseFiles("./templates/main.tmpl")
+	if err != nil {
+		log.Fatal("Error: Unable to parse the template file.")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		if req.URL.Path != "/smatter" {
+		if req.URL.Path == "/smatter" {
+			data := TemplateData {
+				Title: "Do Blue Lives Matter?",
+				Description: "No",
+				URL: "https://doblue.live/smatter",
+			}
+
+			mainTemplate.Execute(w, data)
+		} else if req.URL.Path == "/sreallymatter" {
+			data := TemplateData {
+				Title: "Do Blue Lives Really Matter?",
+				Description: "Still No",
+				URL: "https://doblue.live/sreallymatter",
+			}
+
+			mainTemplate.Execute(w, data)
+		} else {
 			scheme := "http"
 			if req.TLS != nil {
 				scheme += "s"
@@ -17,8 +45,6 @@ func main() {
 			
 			return
 		}
-	
-		http.ServeFile(w, req, "./static/index.html")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
